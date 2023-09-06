6636d875cfcfe1e913b88c2734a0f9fb2c352fcc
valegarcia0 <143621970+valegarcia0@users.noreply.github.com>
2023-09-06 08:38:48 -0600

    Add files via upload

---
+++ Listatareas.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package variables;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Listatareas {
+     public static void main(String[] args) {
+       String tarea1="linea del tiempo";
+       String tarea2="investigacion";
+       String tarea3="raices cuadradas";
+       String tarea4="sumas";
+       String tarea5="imagenes";
+         System.out.println("mi primer tarea es"+tarea1);
+         System.out.println("mi segunda tarea es "+tarea2);
+         System.out.println("mi tercer ytarea es"+tarea3);
+         System.out.println("mi cuarta tarea es"+tarea4);
+         System.out.println("mi quinta tarea es "+tarea5);
+     }
+}
