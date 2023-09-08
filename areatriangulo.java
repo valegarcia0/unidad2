f3625c5fd6c43b2a8e04e319f1a60cfa5b0dd96d
valegarcia0 <143621970+valegarcia0@users.noreply.github.com>
2023-09-08 08:33:53 -0600

    Add files via upload

---
+++ areatriangulo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class areatriangulo {
+      public static void main(String[] args) {
+      int base=50;
+      int altura=10;
+      int area= base*altura/2;
+          System.out.println("el area del triangulo es "+area);
+      
+      }
+    
+}
